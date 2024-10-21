3e71a7cbfa2057d87ec9f63ab522766f00bbd2ad
kolisya20 <y.kolisnichenko@gmail.com>
2024-10-21 12:18:11 +0200

    added input_error

--- consoleHelper/task4.py
+++ consoleHelper/task4.py
@@ -1,30 +1,46 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IndexError:
+            return "Enter the argument for the command."
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found."
+    return inner
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args, contacts):
     if len(args) != 2:
-        return "Invalid command. Usage: add [name] [phone]"
+        raise IndexError
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+@input_error
 def change_contact(args, contacts):
     if len(args) != 2:
-        return "Invalid command. Usage: change [name] [new phone]"
+        raise IndexError
     name, phone = args
     if name in contacts:
         contacts[name] = phone
         return "Contact updated."
-    return "Contact not found."
+    raise KeyError
 
+@input_error
 def show_phone(args, contacts):
     if len(args) != 1:
-        return "Invalid command. Usage: phone [name]"
+        raise IndexError
     name = args[0]
     return contacts.get(name, "Contact not found.")
 
+@input_error
 def show_all(contacts):
     if not contacts:
         return "No contacts found."
